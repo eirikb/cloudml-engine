ecc8fbbc356cdab698ba569acb35e30c5ddcdb38
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-26 06:53:11 +0200

    Added domains to repository

---
+++ repository/src/main/java/no/sintef/cloudml/domain/Keys.java
@@ -0,0 +1,23 @@
+package no.sintef.cloudml.domain;
+
+public class Keys {
+
+	private String accesskeyid;
+	private String secretkey;
+
+	public String getAccesskeyid() {
+		return accesskeyid;
+	}
+
+	public void setAccesskeyid(String accesskeyid) {
+		this.accesskeyid = accesskeyid;
+	}
+
+	public String getSecretkey() {
+		return secretkey;
+	}
+
+	public void setSecretkey(String secretkey) {
+		this.secretkey = secretkey;
+	}
+}
