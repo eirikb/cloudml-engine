4554a08490b233355c77edc7e21ea82d2b67d621
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-02 00:34:26 +0100

    Builds with scala again

--- cloud-connector/src/main/java/no/sintef/cloudml/CloudConnector.java
+++
@@ -1,9 +0,0 @@
-package no.sintef.cloudml;
-
-import java.util.List;
-import no.sintef.cloudml.domain.Instance;
-
-public interface CloudConnector {
-
-	public List<Instance> createInstances(List<Instance> instances);
-}
