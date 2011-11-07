4aae42938a52375d1ff63a7f869df5c45103049d
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-07 11:45:56 +0100

    Tests now actually work, for real

---
+++ cloud-connector/src/test/scala/HelloWorldSpec.scala
@@ -0,0 +1,13 @@
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+
+@RunWith(classOf[JUnitRunner])
+class HelloWorldSpec extends Spec with ShouldMatchers {
+  describe("A Stack") {
+
+    it("should pop values in last-in-first-out order") (pending)
+
+    }
+  }
