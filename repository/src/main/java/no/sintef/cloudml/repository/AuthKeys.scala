886c029183d65892dfeb3719f47b00b52ca95a5b
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-08 13:40:53 +0100

    Repository files... for now

---
+++ repository/src/main/java/no/sintef/cloudml/repository/AuthKeys.scala
@@ -0,0 +1,6 @@
+package no.sintef.cloudml.repository
+
+class AuthKeys(accessKeyc: String, secretKeyc: String) {
+  var accessKey = accessKeyc
+  var secretKey = secretKeyc
+}
---
+++ repository/src/main/java/no/sintef/cloudml/repository/Instance.scala
@@ -0,0 +1,7 @@
+package no.sintef.cloudml.repository
+
+class Instance(groupc: String, keyPairc: String, authKeysc: AuthKeys)  {
+  var group = groupc
+  var keyPair = keyPairc
+  var authKeys : AuthKeys = authKeysc
+}
