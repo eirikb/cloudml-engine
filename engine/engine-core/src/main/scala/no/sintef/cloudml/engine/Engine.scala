089f8906380a18b46c4f5f57b54f1e2e0c87aae1
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-22 23:08:33 +0100

    Forgot some files at work, troublesome

---
+++ engine/engine-core/src/main/scala/no/sintef/cloudml/engine/Engine.scala
@@ -0,0 +1,15 @@
+package no.sintef.cloudml.engine
+
+import no.sintef.cloudml.cloudconnector.Account
+import no.sintef.cloudml.engine.domain._
+import no.sintef.cloudml.repository._
+import no.sintef.cloudml.engine.infrastructure.InfrastructureEngine
+
+object Engine {
+
+    def apply(account: Account, template: Template) {
+       var instances = for (i <- template.nodes) yield Instance("group?", "dev")
+       instances = Repository.magic(instances)
+       InfrastructureEngine(account, instances)
+    }
+}
