3d6be927309fc364bd2df9b3fbc3dfc27e725809
Eirik Brandtzg <eirikb@eirikb.no>
2011-11-22 16:39:46 +0100

    Moving domain classes around

--- engine/infrastructure/src/main/scala/no/sintef/cloudml/engine/infrastructure/InfrastructureEngine.scala
+++ engine/infrastructure/src/main/scala/no/sintef/cloudml/engine/infrastructure/InfrastructureEngine.scala
@@ -1,9 +1,12 @@
 package no.sintef.cloudml.engine.infrastructure
 
-import no.sintef.cloudml.engine.domain.Template
+import no.sintef.cloudml.engine.domain._
+import no.sintef.cloudml.repository._
 
 object InfrastructureEngine {
 
+    // Somethings tells me apply should not be used like this...
     def apply(template: Template) {
+        val instances = for (node <- template.nodes) yield new Instance("group?", "dev")
     }
 }
--- repository/src/main/java/no/sintef/cloudml/repository/Repository.scala
+++ repository/src/main/java/no/sintef/cloudml/repository/Repository.scala
@@ -1,11 +1,6 @@
 package no.sintef.cloudml.repository
 
-import no.sintef.cloudml.engine.domain.Template
 
 object Repository {
 
-    def map(template :Template):  List[Instance] = {
-
-        List(new Instance("group?", "dev"))
-    }
 }
