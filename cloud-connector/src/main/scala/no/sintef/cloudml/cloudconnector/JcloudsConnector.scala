539f7c345fd1618fdf1f7c86208d8aa27ed40b37
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-23 00:11:23 +0100

    It works! At least like this...

--- cloud-connector/src/main/scala/no/sintef/cloudml/cloudconnector/JcloudsConnector.scala
+++ cloud-connector/src/main/scala/no/sintef/cloudml/cloudconnector/JcloudsConnector.scala
@@ -11,12 +11,11 @@ class JcloudsConnector extends CloudConnector {
     def createInstance(account: Account, instance: Instance) {
 
         val authKeys = account.authKeys
-        println("Here should instance" + instance + " be built, accessKeys: " + authKeys)
 
-        //val context = new ComputeServiceContextFactory().createContext("aws-ec2", authKeys.accessKey, authKeys.secretKey)
+        val context = new ComputeServiceContextFactory().createContext("aws-ec2", authKeys.accessKey, authKeys.secretKey)
 
-        //val template = context.getComputeService().templateOptions().as(classOf[AWSEC2TemplateOptions]).securityGroups(instance.group).keyPair(instance.keyPair)
+        val template = context.getComputeService().templateOptions().as(classOf[AWSEC2TemplateOptions]).securityGroups(instance.group).keyPair(instance.keyPair)
 
-        //context.getComputeService().createNodesInGroup("webserver", 2, template)
+        context.getComputeService().createNodesInGroup("webserver", 1, template)
     }
 }
--- engine/engine-core/src/main/scala/no/sintef/cloudml/engine/Engine.scala
+++ engine/engine-core/src/main/scala/no/sintef/cloudml/engine/Engine.scala
@@ -8,7 +8,7 @@ import no.sintef.cloudml.engine.infrastructure.InfrastructureEngine
 object Engine {
 
     def apply(account: Account, template: Template) {
-       var instances = for (i <- template.nodes) yield Instance("group?", "dev")
+       var instances = for (i <- template.nodes) yield Instance("default", "dev")
        instances = Repository.magic(instances)
        InfrastructureEngine(account, instances)
     }
