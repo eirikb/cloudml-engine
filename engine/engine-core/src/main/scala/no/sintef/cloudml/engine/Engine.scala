39dae5d11690f7cb48aa58c5b40155ec9f9243ef
eirikb <eirikb@eirikb.no>
2011-12-11 18:44:11 +0100

    Callchain

--- engine/engine-core/src/main/scala/no/sintef/cloudml/engine/Engine.scala
+++ engine/engine-core/src/main/scala/no/sintef/cloudml/engine/Engine.scala
@@ -1,12 +1,20 @@
 package no.sintef.cloudml.engine
 
+import no.sintef.cloudml.kernel.Kernel
 import no.sintef.cloudml.kernel.domain._
 import no.sintef.cloudml.repository._
 import no.sintef.cloudml.engine.infrastructure.InfrastructureEngine
 
 object Engine {
 
-    def apply(accountJson: String, templateJson: String) {
-           }
+    def apply(accountJson: String, templatesJson: List[String]) {
+        val account  = Kernel.deserializeAccount(accountJson)
+        val templates = templatesJson.map(templateJson =>
+            Kernel.deserializeTemplate(templateJson)
+        ) 
 
+        val instances = Repository.mapping(account, templates)
+
+        InfrastructureEngine.apply(account, instances)
+    }
 }
