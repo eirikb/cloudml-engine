582c0668fc32a2f9c281467daf7e86d2668654eb
eirikb <eirikb@eirikb.no>
2012-01-09 13:43:00 +0100

    LICENSES! Added lgpl to all scala-files. Maintanied by maven-license-plugin

--- kernel/src/main/scala/no/sintef/cloudml/kernel/domain/Template.scala
+++ kernel/src/main/scala/no/sintef/cloudml/kernel/domain/Template.scala
@@ -1,3 +1,25 @@
+/**
+ * This file is part of CloudML
+ *
+ * Copyright (C) 2012-  SINTEF ICT
+ * Contact: Eirik Brandtzæg <eirikb@eirikb.no>
+ *
+ * Module: kernel
+ *
+ * CloudML is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * CloudML is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with CloudML. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package no.sintef.cloudml.kernel.domain
 
 case class Template(nodes: List[Node])
