d78e9283dc14c0b8d90824c7c73c008080b35ff2
eirikb <eirikb@eirikb.no>
2012-01-10 14:10:38 +0100

    Removed bogus code, but keeping dummyclass

--- engine/src/test/scala/no/sintef/cloudml/engine/EngineSpec.scala
+++ engine/src/test/scala/no/sintef/cloudml/engine/EngineSpec.scala
@@ -9,11 +9,4 @@ import no.sintef.cloudml.kernel.domain._
 
 @RunWith(classOf[JUnitRunner])
 class EngineSpec extends Spec with ShouldMatchers {
-    describe("Basic engine testing. the apply") {
-
-      it("should return a list of runtimeinstances when called") {
-        assert(1 == 1)
-        println("LIKE A BOSS")
-      }
-    }
 }
