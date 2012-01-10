ae71b99da7cffd0dd3488b733801ea2ef1ddffe5
eirikb <eirikb@eirikb.no>
2012-01-10 14:23:43 +0100

    Actually testing nodes->instnaces in repository

--- repository/src/test/scala/no/sintef/cloudml/repository/RepositorylSpec.scala
+++ repository/src/test/scala/no/sintef/cloudml/repository/RepositorylSpec.scala
@@ -12,32 +12,11 @@ import no.sintef.cloudml.repository.domain._
 class RepositorySpec extends Spec with ShouldMatchers {
     describe("Mapping") {
         it("should map nodes to instances") {
-            val instances = Repository.mapping(null, Template(List(Node("test1"), Node("test2", 1000), Node("test3", 2000, 5)))
-            println(instances)
-        }
-    }
-        
-        
+            val instances = Repository.mapping(null, List(Template(List(Node("test1"), Node("test2", 1000), Node("test3", 2000, 5)))))
 
-/*
-        it("AWS should be mapped directly") {
-            val account = new Account("aws-ec2", null)
-            val template = new Template(List(new Node(), new Node("Small")))
-            val instances = Repository.mapping(account, List(template))
-            assert(instances.length == 2)
-            assert(instances.head == new AWSInstance("Medium"))
-            assert(instances.last == new AWSInstance("Small"))
-          }
-    
-        it("Rackspace should be mapped to numbers (flavor)") {
-            val account = new Account("cloudservers", null)
-            val template = new Template(List(new Node(), new Node("Small"), new Node("Large")))
-            val instances = Repository.mapping(account, List(template))
-            assert(instances.length == 3)
-            assert(instances.head == new RackspaceInstance(2))
-            assert(instances(1) == new RackspaceInstance(1))
-            assert(instances.last == new RackspaceInstance(3))
+            instances should equal(List(Instance("test1"), Instance("test2", 1000), Instance("test3", 2000, 5)))
+
+            instances should equal(List(Instance("test1", 0, 1), Instance("test2", 1000, 1), Instance("test3", 2000, 5)))
         }
-*/
     }
 }
