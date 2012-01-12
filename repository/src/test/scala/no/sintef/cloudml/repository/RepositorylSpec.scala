eae9d86f3c68caf17aec9cea78c4a16d2fa608db
eirikb <eirikb@eirikb.no>
2012-01-12 10:25:56 +0100

    Reflectiontest for repository

--- repository/src/test/scala/no/sintef/cloudml/repository/RepositorylSpec.scala
+++ repository/src/test/scala/no/sintef/cloudml/repository/RepositorylSpec.scala
@@ -18,5 +18,23 @@ class RepositorySpec extends Spec with ShouldMatchers {
 
             instances should equal(List(Instance("test1", 0, 1), Instance("test2", 1000, 1), Instance("test3", 2000, 5)))
         }
+
+        it("should atm be complete equality of nodes and instances") {
+
+            val nodes = List(Node("test1"), Node("test2", 1000), Node("test3", 2000, 5))
+            val instances = Repository.mapping(null, List(Template(nodes)))
+
+            nodes.zipWithIndex.foreach { case (node, i) =>
+                val instance = instances(i)
+                val fields = node.getClass.getDeclaredFields
+                fields.foreach { field =>
+                    val nMethod = node.getClass.getDeclaredMethod(field.getName)
+                    val nValue = nMethod.invoke(node)
+                    val iMethod = instance.getClass.getDeclaredMethod(field.getName)
+                    val iValue = iMethod.invoke(instance)
+                    nValue should equal(iValue)
+                }
+            }
+        }
     }
 }
