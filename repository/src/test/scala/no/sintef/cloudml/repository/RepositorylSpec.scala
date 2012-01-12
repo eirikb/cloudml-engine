7780ead2dbb0640061668c5ba0cd7c220b4e9b4c
eirikb <eirikb@eirikb.no>
2012-01-12 10:35:21 +0100

    Much better test for repository

--- repository/src/test/scala/no/sintef/cloudml/repository/RepositorylSpec.scala
+++ repository/src/test/scala/no/sintef/cloudml/repository/RepositorylSpec.scala
@@ -12,11 +12,9 @@ import no.sintef.cloudml.repository.domain._
 class RepositorySpec extends Spec with ShouldMatchers {
     describe("Mapping") {
         it("should map nodes to instances") {
-            val instances = Repository.mapping(null, List(Template(List(Node("test1"), Node("test2", 1000), Node("test3", 2000, 5)))))
+            val instances = Repository.mapping(null, List(Template(List(Node("test1"), Node("test2", 1000), Node("test3", 2000, 5), Node("test4", 2000, 5, 1000)))))
 
-            instances should equal(List(Instance("test1"), Instance("test2", 1000), Instance("test3", 2000, 5)))
-
-            instances should equal(List(Instance("test1", 0, 1), Instance("test2", 1000, 1), Instance("test3", 2000, 5)))
+            instances should equal(List(Instance("test1", 0, 1, 0), Instance("test2", 1000, 1, 0), Instance("test3", 2000, 5, 0), Instance("test4", 2000, 5, 1000)))
         }
 
         it("should atm be complete equality of nodes and instances") {
