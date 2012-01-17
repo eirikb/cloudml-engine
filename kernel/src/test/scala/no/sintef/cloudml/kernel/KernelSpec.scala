6dd2b09a4a7fb7aeff8194e1a748b3abffd99709
eirikb <eirikb@eirikb.no>
2012-01-17 09:11:16 +0100

    Updated kernel for specs2

--- kernel/src/test/scala/no/sintef/cloudml/kernel/KernelSpec.scala
+++ kernel/src/test/scala/no/sintef/cloudml/kernel/KernelSpec.scala
@@ -1,41 +1,52 @@
 package no.sintef.cloudml.kernel
 
-import org.scalatest.junit.JUnitRunner
-import org.junit.runner.RunWith
-import org.scalatest.Spec
-import org.scalatest.matchers.ShouldMatchers
+//import org.scalatest.junit.JUnitRunner
+//import org.junit.runner.RunWith
+//import org.scalatest.Spec
+//import org.scalatest.matchers.ShouldMatchers
+//import org.specs2.mutable._
+//import org.scalatest.junit.JUnitRunner
+//import org.junit.runner.RunWith
+import org.specs2.mutable._
 
 import no.sintef.cloudml.kernel.domain._
 
-@RunWith(classOf[JUnitRunner])
-class KernelSpec extends Spec with ShouldMatchers {
-    describe("Testing like a boss") {
-
-        it("desrialilzes templates") {
+//@RunWith(classOf[JUnitRunner])
+class KernelSpec extends SpecificationWithJUnit {
+    "Template in JSON format" should {
+        "serialize to internal format" in {
             val s = """{
                 "nodes": [{
-                    "name": "test1", "minRam": 0, "minCores": 1, "minDisk": 0
+                    "name": "test1", 
+                    "minRam": 0, 
+                    "minCores": 1, 
+                    "minDisk": 0
                 },{
-                    "name": "test2", "minRam": 1000, "minCores": 2, "minDisk": 2000
+                    "name": "test2", 
+                    "minRam": 1000, 
+                    "minCores": 2, 
+                    "minDisk": 2000
                 }]}"""
             val t = Kernel.deserializeTemplate(s)
 
-            assert(t.nodes.length == 2)
-            assert(t == new Template(List(new Node("test1"), new Node("test2", 1000, 2, 2000))))
+            t.nodes.length === 2
+            t mustEqual(new Template(List(new Node("test1"), new Node("test2", 1000, 2, 2000))))
         }
+     }
 
-        it("desrialilzes accounts") {
+     "Account in JSON format" should {
+        "serialize to internal format" in {
             val s = """{
                 "provider": "aws-ec2", 
-                "identity": "accessTest", 
-                "credential": "secretTest"
+                "identity": "identityTest", 
+                "credential": "credentialTest"
                 }"""
             val a = Kernel.deserializeAccount(s)
 
-            assert(a.provider == "aws-ec2")
-            assert(a.identity == "accessTest")
-            assert(a.credential == "secretTest")
-            assert(a == Account("aws-ec2", "accessTest", "secretTest"))
+                a.provider mustEqual("aws-ec2")
+                a.identity mustEqual("identityTest")
+                a.credential mustEqual("credentialTest")
+                a mustEqual(Account("aws-ec2", "identityTest", "credentialTest"))
         }
-    }
-}
+     }
+ }
